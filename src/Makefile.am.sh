6ccf68e2c38357dceed83a8927cac1de12e4cdca
Christian Wiese <chris@opensde.org>
2013-09-13 22:59:39 +0200

    netlink: fix to detect sancus-netlink library

--- src/Makefile.am.sh
+++ src/Makefile.am.sh
@@ -13,4 +13,12 @@ AM_LDFLAGS = \$(SANCUS_CORE_LIBS)
 bin_PROGRAMS = \\
 	$(list *.c | sed -e 's,\.c,,g' | sort -V)
 
+if HAVE_SANCUS_NETLINK
+AM_CFLAGS += \$(SANCUS_NETLINK_CFLAGS)
+AM_LDFLAGS += \$(SANCUS_NETLINK_LIBS)
+
+bin_PROGRAMS += \\
+	$(list netlink/*.c | sed -e 's,\.c,,g' | sort -V)
+
+endif
 EOT
